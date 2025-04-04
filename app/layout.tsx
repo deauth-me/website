102798db9d79d5431f82c03faf25c64dd8d2636d
deauth-me <82866970+deauth-me@users.noreply.github.com>
2025-04-04 14:41:47 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Analytics } from "@vercel/analytics/react"
+
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Sacred Structures",
-  description: "Explore the beauty and history of Catholic architecture around the world",
+  description: "Explore the beauty and history of Sacred Structures around the world",
     generator: 'v0.dev'
 }
 
@@ -28,7 +28,7 @@ export default function RootLayout({
           {children}
        
         </ThemeProvider>
-        <Analytics />
+        
       </body>
     </html>
   )
